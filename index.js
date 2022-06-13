f8c45677fc718541785f667096a40c2ab43e5f5e
Otávio <sedanootavio@gmail.com>
2022-06-13 11:32:59 -0300

    feat: add get infos by id, routes and refact in code

--- index.js
+++ index.js
@@ -12,42 +12,111 @@ async function server() {
 const kittySchema = new mongoose.Schema({
   name: String,
   age: Number,
-  color: String,
+  owner: {type: mongoose.Types.ObjectId},
   createdAt: Date,
   updateAt: Date
 })
 
-const Kitten = mongoose.model('Kitten', kittySchema)
+const Kitten = mongoose.model('kitten', kittySchema)
 
-async function catList() {
-  await server()
-  const kittenGetted = await Kitten.find({})
-  return kittenGetted
-}
-
-async function createCat (name, age, color) {
-
-  const cat = new Kitten(
-    {
-     name: name,
-     age: age,
-     color: color, 
-     createdAt: new Date, 
-     updateAt: new Date 
-    }
-  )
-  await cat.save()
-}
-
-app.get('/news-api/v1/kittens', async (req, res) => {
-  res.json(await catList().catch(err => console.log(err)))
+const ownerSchema = new mongoose.Schema({
+  name: String,
+  kitten: {type: mongoose.Types.ObjectId},
+  createdAt: Date,
+  updateAt: Date
 })
 
-app.post('/news-api/v1/kitten/create/:name/:age/:color', async (req, res) => {
-  const name = req.params.name
-  const age = req.params.age
-  const color = req.params.color
-  res.status(201).json(await createCat(name, age, color))
+const Owner = mongoose.model('owner', ownerSchema)
+
+async function ownerList() {
+  await server()
+  const ownerGetted = await Owner.find({}).populate({path: "kitten", model: "kitten"})
+  return ownerGetted
+}
+
+async function createOwner (ownerName) {
+  const owner = new Owner(
+    {
+      name: ownerName,
+      kitten: null,
+      createdAt: new Date, 
+      updateAt: new Date 
+    }
+  )
+  await owner.save()
+}
+
+async function createKitten (kittenName) {
+
+  const kitten = new Kitten(
+    {
+     name: kittenName,
+     age: 1,
+     createdAt: new Date,
+     updateAt: new Date 
+    },
+  )
+  await kitten.save()
+}
+
+async function getOwner(Id) {
+  await server()
+  const getId = await Owner.findById(Id).populate({path: "kitten", model: "kitten"})
+  return getId
+}
+
+async function getKitten(Id) {
+  await server()
+  const getId = await Kitten.findById(Id).populate({path: "owner", model: "owner"})
+  return getId
+}
+
+async function createRelations(kittenId, ownerId) {
+  await server()
+
+  const addRelations = async () => {
+    await Kitten.findByIdAndUpdate(kittenId, {owner: ownerId}, { 
+      new: true,
+    },)
+    await Owner.findByIdAndUpdate(ownerId, {kitten: kittenId}, { 
+      new: true,
+    })
+  }
+  return await addRelations()
+}
+
+app.get('/news-api/v1/owners', async (req, res) => {
+  res.status(200).json(await ownerList())
+})
+
+app.get('/news-api/v1/owner/:ownerId', async (req, res) => {
+  const ownerId = req.params.ownerId
+  res.status(200).json({ msg: await getOwner(ownerId)})
+})
+
+app.get('/news-api/v1/kitten/:kittenId', async (req, res) => {
+  const kittenId = req.params.kittenId
+
+  res.status(200).json({ msg: await getKitten(kittenId)})
+})
+
+app.post('/news-api/v1/kitten/create/:kittenName', async (req, res) => {
+  const kittenName = req.params.kittenName
+
+  res.status(201).json({msg: await createKitten(kittenName)})
+})
+
+app.post('/news-api/v1/owner/create/:ownerName', async (req, res) => {
+  const ownerName = req.params.ownerName
+
+  res.status(201).json({ msg: await createOwner(ownerName)})
+})
+
+app.post('/news-api/v1/kitten/addRelation/:ownerId/:kittenId', async (req, res) => {
+  const ownerId = req.params.ownerId
+  const kittenId = req.params.kittenId
+
+  res.status(201).json(await createRelations(kittenId, ownerId))
 })
 
 app.listen(port, hostname, () => {
